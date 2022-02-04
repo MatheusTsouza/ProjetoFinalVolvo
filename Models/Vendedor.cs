3f61eb6b03c20fe1cfbb60a292374d4d551d1c3b
Matheus Teixeira de Souza <matheusteixeiradesouza@gmail.com>
2022-02-04 15:53:40 -0300

    adicionando data annotations ao vendedor

--- Models/Vendedor.cs
+++ Models/Vendedor.cs
@@ -3,7 +3,9 @@ namespace ProjetoFinalVolvo
 {
   public class Vendedor
   {
+    [Key]
     public int vendedorId { get; set; }
+    [StringLength(45)]
     public string nome { get; set; }
     public float salario { get; set; }
     public Veiculo? veiculo;
