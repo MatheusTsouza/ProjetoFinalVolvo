3b82d8443740ab3d3111aa793e1b268e9e3c5373
Matheus Teixeira de Souza <matheusteixeiradesouza@gmail.com>
2022-02-04 13:43:58 -0300

    adicionando arquivo de configuração da conexão com o sql server

--- Models/ConcessionariaContexto.cs
+++ Models/ConcessionariaContexto.cs
@@ -3,14 +3,15 @@ namespace ProjetoFinalVolvo
 {
   public class ConcessionariaContexto : DbContext
   {
+
     public DbSet<Veiculo> Veiculos { get; set; } = null!;
     public DbSet<Vendedor> Vendedores { get; set; } = null!;
     public DbSet<Proprietario> Proprietarios { get; set; } = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-		string abc = @".\;Database=Concessionaria;Trusted_Connection=True";
-		optionsBuilder.UseSqlServer(@"Server=" + abc);
+      string abc = System.IO.File.ReadAllText("Models/app.config");
+      optionsBuilder.UseSqlServer(abc);
     }
   }
 }
